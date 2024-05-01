c893bf8424cb8a6a5b8c941d132b8ebca52ad3b5
AlejandroFadim <alejandro.fadim@sptech.school>
2024-04-30 23:01:27 -0300

    criação do campo de comentarios

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,33 @@
 var logBtn = document.getElementById("btnLogin");
 var avatar = document.getElementById("avatar");
+var spanComentario = document.getElementById("spanComentario")
+var LinkComentarioLogin = document.getElementById("LinkComentario")
 
 function pegaTags() {
 
     if (sessionStorage.EMAIL_USUARIO) {
-    logBtn.style.display = "none";
-    avatar.style.display = "block";
-    avatar.style.backgroundColor = "green";
-    avatar.innerHTML = sessionStorage.NOME_USUARIO;
-    console.log("usuario existe");
+        logBtn.style.display = "none";
+        avatar.style.display = "block";
+        avatar.style.backgroundColor = "green";
+        avatar.innerHTML = sessionStorage.NOME_USUARIO;
+        console.log("usuario existe");
     } else {
-    console.log("nao tem usuario");
+        console.log("nao tem usuario");
     }
 }
+
+function comentar(){
+    if(sessionStorage.EMAIL_USUARIO) {
+        console.log('pode comentar')
+    }
+    else {
+        spanComentario.style.display = "block";
+        LinkComentarioLogin.style.display = "block";
+        console.log(essionStorage.EMAIL_USUARIO) 
+    } 
+
+}
+
 
 
 pegaTags();
